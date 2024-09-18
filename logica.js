a2bc04d86ed9ab3a4c1878b46d79c8fcdaa159c3
Ninth00 <marquezhernandezmariogabriel@gmail.com>
2024-09-18 11:47:58 -0600

    DNI

---
+++ logica.js
@@ -0,0 +1,31 @@
+
+
+document.getElementById('dniForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+    var dniNumber = document.getElementById('dniNumber').value;
+    var dniLetter = document.getElementById('dniLetter').value.toUpperCase();
+    
+    // Validar que el número de DNI esté en el rango permitido
+    if (dniNumber < 0 || dniNumber > 99999999 || dniNumber.length < 1 || dniNumber.length > 8) {
+        alert("El número de DNI proporcionado no es válido.");
+        return;
+    }
+
+    // Validar que la letra del DNI sea una sola letra y no esté vacía
+    if (dniLetter.length !== 1 || !isNaN(dniLetter)) {
+        alert("La letra del DNI no es válida. Debe ser una letra.");
+        return;
+    }
+
+    // Calcular la letra correcta
+    var letraCalculada = letras[dniNumber % 23];
+
+    // Comparar con la letra ingresada por el usuario
+    if (letraCalculada !== dniLetter) {
+        alert("La letra indicada no es correcta.");
+    } else {
+        alert("El número y la letra del DNI son correctos.");
+    }
+});
