d2457757c509f6bc3c449e0e63ce9484ebf27bc0
Ninth00 <marquezhernandezmariogabriel@gmail.com>
2024-09-18 20:04:36 -0600

    Update logica.js

--- logica.js
+++ logica.js
@@ -9,12 +9,12 @@ document.getElementById('dniForm').addEventListener('submit', function (e) {
     
     // Validar que el número de DNI esté en el rango permitido
     if (dniNumber < 0 || dniNumber > 99999999 || dniNumber.length < 1 || dniNumber.length > 8) {
-        alert("El número de DNI proporcionado no es válido.");
+        alert("El número de DNI proporcionado no es válido." + \n + "El formato es moyor que cero y menor que 99999999");
         return;
     }
 
     // Validar que la letra del DNI sea una sola letra y no esté vacía
-    if (dniLetter.length !== 1 || !isNaN(dniLetter)) {
+    if (dniLetter.length > 1 || !isNaN(dniLetter)) {
         alert("La letra del DNI no es válida. Debe ser una letra.");
         return;
     }
